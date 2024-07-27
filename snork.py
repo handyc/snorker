e1502ed4eac09b0e785fc0b6260e33184a226a0b
Christopher Handy <handyc@helxt.com>
2024-07-27 22:00:48 +0200

    Update snork.py
    small improvement

--- snork.py
+++ snork.py
@@ -1,22 +1,20 @@
 import time, sys
 
 snorker = { }
-head = int(time.time())
+head = int(time.time()*1000.0)
 snorker[head] = (head, head)
 
 if(len(sys.argv) > 1):
-    last = [*snorker.keys()][-1]
     snork = sys.argv[1]
-    now = int(time.time())
+    now = int(time.time()*1000.0)
     snorker[now] = (snork, head)
-    snorker[last] = (snorker[last][0], now)
     for item in snorker:
         print(str(item) + ": " + str(snorker[item]))
 
 while(1):
     last = [*snorker.keys()][-1]
     snork = input("Snork value: ")
-    now = int(time.time())
+    now = int(time.time()*1000.0)
     snorker[now] = (snork, head)
     snorker[last] = (snorker[last][0], now)
     for item in snorker:
