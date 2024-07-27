41e6909d0c5c699e821c3d036651c3b1a04990da
Christopher Handy <handyc@helxt.com>
2024-07-27 17:40:18 +0200

    Update snork.py
    optional argument

--- snork.py
+++ snork.py
@@ -1,8 +1,17 @@
-import time
+import time, sys
 
 snorker = { }
 head = int(time.time())
 snorker[head] = (head, head)
+
+if(len(sys.argv) > 1):
+    last = [*snorker.keys()][-1]
+    snork = sys.argv[1]
+    now = int(time.time())
+    snorker[now] = (snork, head)
+    snorker[last] = (snorker[last][0], now)
+    for item in snorker:
+        print(str(item) + ": " + str(snorker[item]))
 
 while(1):
     last = [*snorker.keys()][-1]
