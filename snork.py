797a155e5b744b10c0d24f85f70b8ea45c5c7fd0
Christopher Handy <handyc@helxt.com>
2024-07-27 17:26:29 +0200

    Update snork.py
    small display improvement

--- snork.py
+++ snork.py
@@ -8,7 +8,7 @@ while(1):
     last = [*snorker.keys()][-1]
     snork = input("Snork value: ")
     now = int(time.time())
-    print(snork)
     snorker[now] = (snork, head)
     snorker[last] = (snorker[last][0], now)
-    print(snorker)
+    for item in snorker:
+        print(str(item) + ": " + str(snorker[item]))
